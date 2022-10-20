b7f4c16d4a596876270ae58329e0c115bf2c9b6b
AmilaViduranga <amilavidurangawelikala@gmail.com>
2022-10-20 17:27:35 +0530

    W002: Add geo search and map, remove angular restriction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { AgmCoreModule } from '@agm/core';
+
 import { AppComponent } from './app.component';
+import { GoogleMapComponent } from './google-map/google-map.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleMapComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: environment.googleMapApiKey,
+      libraries: ['places']
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/google-map/google-map.component.ts
@@ -0,0 +1,60 @@
+import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import { MapsAPILoader } from '@agm/core';
+import { Location } from '../DAO/location.dao';
+
+@Component({
+  selector: 'app-google-map',
+  templateUrl: './google-map.component.html',
+  styleUrls: ['./google-map.component.css']
+})
+export class GoogleMapComponent implements OnInit {
+
+  mapLocation: Location = {
+    latitude: 0.0,
+    logitude: 0.0
+  };
+  zoom?: number;
+  geoCoder: google.maps.Geocoder;
+  autocomplete: google.maps.places.Autocomplete;
+
+  @ViewChild('search')
+  searchElementRef?: ElementRef;
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) { }
+  
+  ngOnInit() {
+    this.loadGoogleMapAndGEOSearch();
+  }
+
+  private loadGoogleMapAndGEOSearch(): void {
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+      this.geoCoder = new google.maps.Geocoder;
+      this.autocomplete = new google.maps.places.Autocomplete(this.searchElementRef?.nativeElement);
+      this.autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          let place: google.maps.places.PlaceResult = this.autocomplete.getPlace();
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+          this.mapLocation.latitude = place.geometry.location.lat();
+          this.mapLocation.logitude = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
+  }
+
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.mapLocation.latitude = position.coords.latitude;
+        this.mapLocation.logitude = position.coords.longitude;
+        this.zoom = 8;
+      });
+    }
+  }
+}
